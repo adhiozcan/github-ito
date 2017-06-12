492b4b8d90eefccf9643b1129964b3fdec500081
adhiozcan <wahyuad10541@gmail.com>
2017-06-12 11:33:19 +0700

    Remove unused resource.

--- app/src/main/java/id/net/iconpln/apps/ito/ui/fragment/PelaksanaanFragment.java
+++ app/src/main/java/id/net/iconpln/apps/ito/ui/fragment/PelaksanaanFragment.java
@@ -92,8 +92,7 @@ public class PelaksanaanFragment extends Fragment implements OnMapReadyCallback 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_pelaksanaan2, container, false);
+        view = inflater.inflate(R.layout.fragment_pelaksanaan, container, false);
         initView();
         initMap();
         //provideDummyData();
