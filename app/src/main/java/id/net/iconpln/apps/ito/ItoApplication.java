1087bf1b3998191488ea490f217452027a705c30
adhiozcan <wahyuad10541@gmail.com>
2017-07-10 09:57:27 +0700

    SImplify Realm init using LocalDb function, Add several Constants, Update several library version

--- app/src/main/java/id/net/iconpln/apps/ito/ItoApplication.java
+++ app/src/main/java/id/net/iconpln/apps/ito/ItoApplication.java
@@ -1,24 +1,15 @@
 package id.net.iconpln.apps.ito;
 
-import android.Manifest;
 import android.app.Application;
-import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
-import com.facebook.stetho.Stetho;
-import com.facebook.stetho.okhttp3.StethoInterceptor;
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
 import java.util.Locale;
 
-import id.net.iconpln.apps.ito.helper.CheckPermission;
 import id.net.iconpln.apps.ito.socket.ItoHttpClient;
-import id.net.iconpln.apps.ito.socket.SocketTransaction;
 import id.net.iconpln.apps.ito.storage.ItoStorage;
+import id.net.iconpln.apps.ito.storage.LocalDb;
 import io.fabric.sdk.android.Fabric;
-import io.realm.Realm;
-import io.realm.RealmConfiguration;
-import okhttp3.OkHttpClient;
 
 /**
  * Created by Ozcan on 23/03/2017.
@@ -35,19 +26,6 @@ public class ItoApplication extends Application {
         Fabric.with(this, new Crashlytics());
         ItoStorage.init(this);
         ItoHttpClient.init(this);
-        RealmConfiguration config = new RealmConfiguration
-                .Builder(getApplicationContext())
-                .deleteRealmIfMigrationNeeded()
-                .build();
-        Realm.setDefaultConfiguration(config);
-
-        Stetho.initialize(
-                Stetho.newInitializerBuilder(getApplicationContext())
-                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(getApplicationContext()))
-                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(getApplicationContext()).build())
-                        .build());
-        new OkHttpClient.Builder()
-                .addNetworkInterceptor(new StethoInterceptor())
-                .build();
+        LocalDb.setupRealm(this);
     }
 }
--- app/src/main/java/id/net/iconpln/apps/ito/helper/Constants.java
+++ app/src/main/java/id/net/iconpln/apps/ito/helper/Constants.java
@@ -6,10 +6,12 @@ package id.net.iconpln.apps.ito.helper;
 
 public class Constants {
     // for login configuration
-    public static final String IS_REMEMBER = "app.ito.is_remember";
-    public static final String SAVE_USER   = "app.ito.user_save";
-    public static final String USERNAME    = "app.ito.username";
-    public static final String PASSWORD    = "app.ito.password";
+    public static final String IS_REMEMBER  = "app.ito.is_remember";
+    public static final String SAVE_USER    = "app.ito.user_save";
+    public static final String USERNAME     = "app.ito.username";
+    public static final String PASSWORD     = "app.ito.password";
+    public static final String KODE_PETUGAS = "app.ito.kode_petugas";
+    public static final String ID_UNIT_UP   = "app.ito.id_unit_up";
 
     // for network purpose
     public static final String SOCKET_FAILURE         = "app.ito.socket_failure";
